8fb1f5fb9e35f1c012b6979ea6e9116a0f047ab6
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-10 19:24:15 +1000

    made dino game, setup floor and dino and animations

---
+++ Scripts/dinoground.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+const SPEED = -200
+var screen_width: float
+func _ready() -> void:
+	screen_width = get_viewport_rect().size.x
+	print (screen_width)
+func _physics_process(delta: float) -> void:
+
+		global_position.x += SPEED * delta
+		
+		if global_position.x < -500:
+		# Teleport it back to the right side of the screen
+			global_position.x = screen_width + 621
+			
