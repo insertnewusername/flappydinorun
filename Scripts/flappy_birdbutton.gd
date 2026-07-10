ed040e94723a179059be6592db00a4d10afdc726
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-10 11:44:33 +1000

    add changescene when hover over button and space pressed

---
+++ Scripts/flappy_birdbutton.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	if is_hovered() and Input.is_action_just_pressed("Space"):
+		get_tree().change_scene_to_file("res://Sprites/flappybird.tscn")
