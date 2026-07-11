8a939df9e1462731631c16bd05690e18efc5a5a4
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-11 13:31:24 +1000

    added fade to dino game

--- Scripts/dinogame.gd
+++ Scripts/dinogame.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var fade_transition: ColorRect = $Fade_transition
-
+var backhomes = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +13,14 @@ func _process(delta: float) -> void:
 
 func _on_fade_timer_timeout() -> void:
 	$Fade_transition.hide()
+	if backhomes == 1:
+		get_tree().change_scene_to_file("res://Sprites/Start.tscn")
+	
+func backhome() -> void:
+	backhomes += 1
+	print(backhomes)
+	$Fade_transition/Fade_timer.start()
+	$Fade_transition.show()
+	$Fade_transition/AnimationPlayer.play("fade_out")
+	
+	
