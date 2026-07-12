5b429e037473d0fd5612397304604c18560cb765
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-13 09:56:16 +1000

    let dino triple jump

--- Scripts/dino.gd
+++ Scripts/dino.gd
@@ -6,8 +6,9 @@ extends CharacterBody2D
 
 var immune = 0
 var stop = 0
+var onfloor = 0
 
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -230.0
 var score = 0
 func _ready() -> void:
 	label.text = "Hi: %d   Score: %d" % [Global.hiscore, score]
@@ -17,13 +18,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and onfloor < 2:
 		velocity.y = JUMP_VELOCITY
 		jump.play()
+		onfloor += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-
+	if is_on_floor():
+		onfloor = 0
 	move_and_slide()
 
 func addscore():
